bada9f490aba1f2c4f9d71085b98846091bc61e1
Astriex <melishuskanovic@hotmail.com>
2021-05-04 10:15:28 +0200

    implemented details screen

--- app/src/main/java/com/astriex/catsvsdogs/ui/activities/MainActivity.kt
+++ app/src/main/java/com/astriex/catsvsdogs/ui/activities/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.astriex.catsvsdogs.R
 import com.astriex.catsvsdogs.databinding.ActivityMainBinding
@@ -25,6 +26,11 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.findNavController()
         binding.bottomNavigation.setupWithNavController(navController)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 }
--- app/src/main/java/com/astriex/catsvsdogs/ui/fragments/home/details/DetailsFragment.kt
+++ app/src/main/java/com/astriex/catsvsdogs/ui/fragments/home/details/DetailsFragment.kt
@@ -1,14 +1,27 @@
 package com.astriex.catsvsdogs.ui.fragments.home.details
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.astriex.catsvsdogs.R
+import com.astriex.catsvsdogs.databinding.FragmentDetailsBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class DetailsFragment : Fragment(R.layout.fragment_details) {
+    private val args by navArgs<DetailsFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,4 +31,54 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
         return inflater.inflate(R.layout.fragment_details, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentDetailsBinding.bind(view)
+
+        binding.apply {
+            val photo = args.photo
+            Glide.with(this@DetailsFragment)
+                .load(photo.urls.regular)
+                .error(R.drawable.ic_error)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        tvCreator.isVisible = true
+                        tvDescription.isVisible = photo.description != null
+                        return false
+                    }
+
+                })
+                .into(ivPhoto)
+
+            tvDescription.text = photo.description
+
+            val uri = Uri.parse(photo.user.attributionUrl)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+
+            tvCreator.apply {
+                text = "Photo by ${photo.user.name} on Unsplash"
+                setOnClickListener {
+                    context.startActivity(intent)
+                }
+                paint.isUnderlineText = true
+            }
+        }
+    }
 }
